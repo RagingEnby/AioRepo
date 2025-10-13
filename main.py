855a386361836745ae8f1906aeffbacee4fae562
RagingEnby <ragingenby@gmail.com>
2025-10-13 02:15:07 -0400

    IT FUCKING WORKKKSSSS

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def main():
             apps=list(filtered_apps.values()),
             news=[],
         )
-        await write("repo6.json", aio_source.to_dict())  # type: ignore
+        await write("repo.json", aio_source.to_dict())  # type: ignore
     finally:
         await asyncreqs.close()
 
